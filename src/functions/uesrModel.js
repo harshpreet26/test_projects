2a19bdb36654ca83beb9aa630c63757a2c5043ea
harshpreet26 <harshmaur2004@gmail.com>
2025-10-06 18:54:59 -0600

    commit

--- src/functions/uesrModel.js
+++ src/functions/uesrModel.js
@@ -1,11 +1,23 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Creating a user schema
 const userSchema = new mongoose.Schema({
-    firstName: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-    age: { type: Number, min: 7, max: 120 }
+  firstName: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  age: { type: Number, min: 7, max: 120 },
 });
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
+
+//Controller functions for user operations
+exports.createUser = async (firstName, email, password, age) => {
+  try {
+    const newUser = new User({ firstName, email, password, age });
+    await newUser.save();
+  } catch (error) {
+    throw new Error("Error creating user: " + error.message);
+  }
+};
+
+//not needed: module.exports = {};
