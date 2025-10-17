27ba330e7af093d58b0f5c4b34e128fa497360eb
harshpreet26 <harshmaur2004@gmail.com>
2025-10-16 18:28:25 -0600

    Updaate

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,13 @@
 describe('template spec', () => {
-  it('passes', () => {
+  it('Gets, types, and asserts', () => {
     cy.visit('https://example.cypress.io')
+
+    cy.contains('type').click()
+
+    cy.url().should('include', '/commands/actions')
+    cy.get('.action-email').type('fakeemail@gmail.com')
+
+
+    cy.get('.action-email').should('have.value', 'fakeemail@gmail.com')
   })
 })
